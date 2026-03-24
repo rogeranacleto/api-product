5e35fdda65e22f82e3df21077b6855c1e369dd36
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-03-23 22:58:52 -0300

    feat: setup main application with routes and dependencies

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"api-product/controller"
+	"api-product/db"
+	"api-product/repository"
+	"api-product/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	ProductController := controller.NewProductControler(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+	server.POST("/product", ProductController.CreateProduct)
+	server.GET("/product/:productId", ProductController.GetProductById)
+	server.Run(":8000")
+}
