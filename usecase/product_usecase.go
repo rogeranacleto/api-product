f0ea7034224dab3b149122623301d1aca76ea302
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-03-23 22:56:55 -0300

    feat: implement product usecase layer

---
+++ usecase/product_usecase.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"api-product/model"
+	"api-product/repository"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.repository.CreateProduct(product)
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+	return product, nil
+}
+
+func (pu *ProductUsecase) GetProductById(id_product int) (*model.Product, error) {
+	product, err := pu.repository.GetProductById(id_product)
+
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
